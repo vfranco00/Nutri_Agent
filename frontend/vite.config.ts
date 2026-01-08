7e0e239a7e6b6413666cedc46608092af668eb53
Victor Franco <franco@Francos-MacBook-Air.local>
2026-01-08 18:09:58 -0300

    chore(connection): supabase connection + requirements + docker changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    fs: {
+      cachedChecks: false // <--- ESSENCIAL PARA O MAC NÃO DAR ERRO 35
+    }
+  }
 })
